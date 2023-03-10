8c1d63efad09d58ebeb203354451dffbe5971376
Lars van Braam <lars@mediamonks.com>
2023-03-10 19:32:29 +0100

    Add the white select

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -7,7 +7,7 @@ import {
   MenuItem,
   Select,
   SelectChangeEvent,
-  styled,
+  ThemeProvider,
   Toolbar,
   Typography,
 } from '@mui/material';
@@ -15,6 +15,7 @@ import GoogleIcon from '@mui/icons-material/Google';
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import * as React from 'react';
 import { useCallback, useState } from 'react';
+import { darkTheme } from '../../theme';
 
 type NavigationProps = {
   onFullRouteClick():void
@@ -25,21 +26,6 @@ export const enum MapSize {
   Cover = 'cover',
   Fixed = 'fixed',
 }
-
-const StyledFormControl = styled(FormControl)`
-  .MuiFormLabel-root,
-  .MuiInputBase-root {
-    color: ${props => props.theme.palette.secondary.main};
-  }
-
-  .MuiInputBase-root {
-    background-color: rgba(255,255,255,.1);
-    
-    &:hover {
-      background-color: rgba(255,255,255,.15);
-    }
-  }
-`
 
 function Navigation({ onFullRouteClick, onMapSizeChange }:NavigationProps) {
   const [mapSize, setMapSize] = useState(MapSize.Cover);
@@ -53,42 +39,44 @@ function Navigation({ onFullRouteClick, onMapSizeChange }:NavigationProps) {
 
 
   return <AppBar  position="relative" >
-    <Toolbar>
-      <Box sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1}}>
-        <Typography variant="h5" component="h1" >
-          <strong>Road Trip Visualizer</strong> | <small>Amerika 2023</small>
-        </Typography>
-      </Box>
-      <Button
-        color="inherit"
-        href={`https://docs.google.com/spreadsheets/d/${process.env.REACT_APP_SHEET_ID}`}
-        target="_blank"
-        startIcon={<GoogleIcon />}
-        sx={{ mr: 1 }}
-      >
-        Source Sheet
-      </Button>
-      <Button
-        color="inherit"
-        onClick={onFullRouteClick}
-        startIcon={<FormatListBulletedIcon />}
-        sx={{ mr: 1 }}
-      >
-        Full route details
-      </Button>
-      <StyledFormControl variant="filled" size="small" sx={{ minWidth: 200 }} color="warning">
-        <InputLabel id="demo-simple-select-label">Map size</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          value={mapSize}
-          label="Age"
-          onChange={onSizeChange}
+    <ThemeProvider theme={darkTheme}>
+      <Toolbar>
+        <Box sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1}}>
+          <Typography variant="h5" component="h1" >
+            <strong>Road Trip Visualizer</strong> | <small>Amerika 2023</small>
+          </Typography>
+        </Box>
+        <Button
+          color="inherit"
+          href={`https://docs.google.com/spreadsheets/d/${process.env.REACT_APP_SHEET_ID}`}
+          target="_blank"
+          startIcon={<GoogleIcon />}
+          sx={{ mr: 1 }}
         >
-          <MenuItem value={MapSize.Fixed}>Fixed size</MenuItem>
-          <MenuItem value={MapSize.Cover}>Cover</MenuItem>
-        </Select>
-      </StyledFormControl>
-    </Toolbar>
+          Source Sheet
+        </Button>
+        <Button
+          color="inherit"
+          onClick={onFullRouteClick}
+          startIcon={<FormatListBulletedIcon />}
+          sx={{ mr: 1 }}
+        >
+          Full route details
+        </Button>
+        <FormControl variant="outlined" size="small" sx={{ minWidth: 200 }}>
+          <InputLabel id="demo-simple-select-label">Map size</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            value={mapSize}
+            label="Map size"
+            onChange={onSizeChange}
+          >
+            <MenuItem value={MapSize.Fixed}>Fixed size</MenuItem>
+            <MenuItem value={MapSize.Cover}>Cover</MenuItem>
+          </Select>
+        </FormControl>
+      </Toolbar>
+    </ThemeProvider>
   </AppBar>
 }
 
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,18 +1,47 @@
 import { createTheme } from '@mui/material';
 
+declare module '@mui/material/styles' {
+  // interface Palette {
+  //   white: Palette['primary'];
+  // }
+  //
+  // interface PaletteOptions {
+  //   white: PaletteOptions['primary'];
+  // }
+}
+
+declare module '@mui/material/FormControl' {
+  // interface FormControlPropsColorOverrides {
+  //   white: true
+  // }
+}
+
+declare module '@mui/material/Input' {
+  // interface InputLabelPropsColorOverrides {
+  //   white: true
+  // }
+}
+
+
 export const theme = createTheme({
   palette: {
     primary: {
-      main: '#010326'
+      main: '#023E73'
     },
     secondary: {
-      main: '#F2EEEB'
+      main: '#F2B90F'
     },
   },
   components: {
-    // Name of the component
     MuiFormControl: {
-      // variants:
-    },
+      styleOverrides: {  }
+    }
+  }
+});
+
+export const darkTheme = createTheme({
+  ...theme,
+  palette: {
+    mode: 'dark',
   },
 });
